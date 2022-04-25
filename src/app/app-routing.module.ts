66d130410ff70e8e5c4ab8c2072fd71159296300
paublas <paublas98@gmail.com>
2022-04-25 20:53:56 +0200

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
-import { NanadenadaComponent } from './components/nanadenada/nanadenada.component';
 import { ProjectComponent } from './components/project/project.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 
 const routes: Routes = [
-  { path: 'nanadenada', component: NanadenadaComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'about', component: AboutComponent },
   { path: 'project', component: ProjectComponent},
-  { path: '',   redirectTo: '/nanadenada', pathMatch: 'full' },
+  { path: '',   redirectTo: '/projects', pathMatch: 'full' },
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
-import { NanadenadaComponent } from './components/nanadenada/nanadenada.component';
 import { ProjectComponent } from './components/project/project.component';
 import {HttpClientModule}  from '@angular/common/http'
 import { ClipboardModule } from "@angular/cdk/clipboard";
+import { CommonModule } from '@angular/common';
 
   @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     ProjectsComponent,
-    NanadenadaComponent,
     ProjectComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ClipboardModule
+    ClipboardModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
